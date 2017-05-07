8c1b3520b4640f3d7c786a86c2a1c294964265a2
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-07 02:25:37 -0700

    move to pm2

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-var Promise       = require('bluebird')
-  , child_process = require('child_process')
+var Promise     = require('bluebird')
+  , winston     = require('winston')
+  , pm2         = require('pm2')
+  , pm2_config  = require('./breadboard.json')
   ;
 
 
-let 
+// let 
     // redis     = child_process.spawn('docker', ['run', 'redis'])
-  //
-    gateway   = child_process.fork('./node_modules/taskmill-core-gateway/index.js')
-  , agent     = child_process.fork('./node_modules/taskmill-core-agent/index.js')
-  , logs      = child_process.fork('./node_modules/taskmill-core-logs/index.js')
-  , account   = child_process.fork('./node_modules/taskmill-core-account/index.js')
-  , codedb    = child_process.fork('./node_modules/taskmill-core-codedb/index.js')
-  ;
-
-// redis.stdout.pipe(process.stdout);
-// redis.stderr.pipe(process.stderr);
 
 require('./lib/rest').listen();
 
-process.on('exit', (code) => {
-  // redis.kill();
-
-  gateway.kill();
-  agent.kill();
-  logs.kill();
-  codedb.kill();
-  account.kill();
-});
+Promise
+  .fromCallback((cb) => pm2.connect(cb))
+  .then(() => {
+    return Promise
+            .fromCallback((cb) => pm2.start(pm2_config, cb));
+  })
+  .then(() => {
+    // pm2.launchBus((err, bus) => {
+    //   bus.on('log:out', (data) => {
+    //   // bus.on('log:PM2', (data) => {
+    //     console.log(data);
+    //   });
+    // });
+  })
+  .catch((err) => {
+    winston.error(err);
+  })
+  .finally(() => {
+    pm2.disconnect();
+  });
