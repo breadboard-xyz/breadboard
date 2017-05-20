f3df4569fb7ee8a6fa219b82680c95fa0561983b
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-19 22:34:57 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 var Promise     = require('bluebird')
   , winston     = require('winston')
-  , pm2         = require('pm2')
-  , pm2_config  = require('./breadboard.json')
   ;
 
-
-// let 
-    // redis     = child_process.spawn('docker', ['run', 'redis'])
-
 require('./lib/rest').listen();
-
-// Promise
-//   .fromCallback((cb) => pm2.connect(cb))
-//   .then(() => {
-//     return Promise
-//             .fromCallback((cb) => pm2.start(pm2_config, cb));
-//   })
-//   .then(() => {
-//     // pm2.launchBus((err, bus) => {
-//     //   bus.on('log:out', (data) => {
-//     //   // bus.on('log:PM2', (data) => {
-//     //     console.log(data);
-//     //   });
-//     // });
-//   })
-//   .catch((err) => {
-//     winston.error(err);
-//   })
-//   .finally(() => {
-//     pm2.disconnect();
-//   });
