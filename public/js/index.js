476c9e6f9ce3eb8f59a58733189493e847270874
Ahmed Kamel <ahmed.kamel@gmail.com>
2018-12-01 04:39:36 -0800

    rebuild terminal with proper fit function / might still be broken on rendering

--- public/js/index.js
+++
@@ -1,3 +0,0 @@
-// import { Breadboard } from '../../node_modules/breadboard-sdk-web/dist/umd.js'
-//
-// export default Breadboard.default;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-// const path = require('path');
-//
-// module.exports = {
-//   entry: './public/js/index.js',
-//   output: {
-//     libraryTarget: 'var',
-//     library: 'Breadboard',
-//     // libraryExport: 'Breadboard',
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'public/js')
-//   }
-// };
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+  entry: './public/entry.js',
+  output: {
+    path: path.resolve('')
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './node_modules/breadboard-sdk-web/dist/sdk.js', to: 'public/js/bundle.js' },
+    ], {})
+  ]
+}
+
+module.exports = [config];
